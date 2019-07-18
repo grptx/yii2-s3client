b57fd7ce6bf2f9b1ec3f43f18caf2f5d03d8d1b6
roberto <roberto.rossetti@unicaspa.it>
2019-07-18 10:34:02 +0200

    TBC

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # yii2-S3Client
+
+Yii2 S3Client based on [klinson/aws-s3-minio](https://github.com/klinson/aws-s3-minio)
+
+TBC
--- src/S3Client.php
+++ src/S3Client.php
@@ -2,7 +2,45 @@
 
 namespace grptx;
 
-class S3Client
+use yii\base\Component;
+
+class S3Client extends Component
 {
+    /** @var \Aws\S3\S3Client */
+    private $S3Client;
+
+    public $key;
+
+    public $secret;
+
+    public $region;
+
+    public $version;
+
+    public $endpoint;
+
+    public $use_path_style_endpoint = true;
+
+    /**
+     * @return \Aws\S3\S3Client
+     */
+    public function getS3Client(): \Aws\S3\S3Client
+    {
+        if(!$this->S3Client) {
+            $this->S3Client = new \Aws\S3\S3Client([
+                'credentials' => [
+                    'key'    => $this->key ?? '',
+                    'secret' => $this->secret ?? '',
+                ],
+                'region' => $this->region ?? '',
+                'version' => $this->version ?? '',
+                'endpoint' => $this->endpoint ?? '',
+                'use_path_style_endpoint' => true,
+            ]);
+        }
+
+        return $this->S3Client;
+    }
+
 
 }
